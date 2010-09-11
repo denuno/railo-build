228f69cb057c9991e8f49dbc3d941923ea3169e2
Denny Valliant <valliant@unm.edu>
2010-09-11 10:43:25 -0600

    adding build files

---
+++ build/cfdistro.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+export ANT_HOME="/workspace/railo-build/build/cfdistro/ant/"
+/workspace/railo-build/build/cfdistro/ant/bin/ant -f build.xml $*
+		
+		
---
+++ build/start.bat
@@ -0,0 +1,1 @@
+java -Xmx200m -jar "/workspace/railo-build/build/cfdistro/lib/jetty-runner-7.1.0.v20100505.jar" --port 8181 --path / %1 "/workspace/railo-build/build/temp/railo-build.war"
