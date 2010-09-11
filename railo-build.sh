c6e5e80a19e01b6ae3e2c6350864991ab3c7bfa9
Denny Valliant <valliant@unm.edu>
2010-09-11 10:52:51 -0600

    Adding project file and scripts

---
+++ railo-build.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+export ANT_HOME="build/cfdistro/ant/"
+if [ -z "$1" ]; then
+echo "railo-build control script"
+OPTIONS="start stop help list-targets update exit"
+select opt in $OPTIONS; do
+if [ "$opt" = "start" ]; then
+	build/cfdistro/ant/bin/ant -f build/build.xml build.start.launch
+	exit
+elif [ "$opt" = "stop" ]; then
+	build/cfdistro/ant/bin/ant -f build/build.xml server.stop
+	exit
+elif [ "$opt" = "help" ]; then
+	echo "usage (skips this prompt): railo-build.sh [start|stop|{target}]"
+elif [ "$opt" = "list-targets" ]; then
+	build/cfdistro/ant/bin/ant -f build/build.xml help
+elif [ "$opt" = "update" ]; then
+	build/cfdistro/ant/bin/ant -f build/build.xml project.update
+elif [ "$opt" = "exit" ]; then
+	exit
+else
+	#clear
+	echo bad option
+fi
+done
+fi
+reldir=`dirname $0`
+$reldir/build/cfdistro/ant/bin/ant -f $reldir/build/build.xml $*
+	
+	
